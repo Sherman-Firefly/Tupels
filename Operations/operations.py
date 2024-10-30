5b374e9789a00a2ad1d6790b133a246e3ab73ee6
Sherman-Firefly <efracw2007@gmail.com>
2024-10-30 17:16:43 +0800

    tuples

---
+++ Operations/operations.py
@@ -0,0 +1,13 @@
+tuple=("Yes", 2, 2.11, "No")
+print(tuple)
+print(len(tuple))
+print(tuple[2])
+
+tuplea=(1,4,2,3,5)
+print(tuple+tuplea)
+
+tuplec=(4,4,6,5,1,2,3,4,8)
+print(tuplec.count(4))
+
+tupled=("A", "B", "C", "D", "E", "F")
+print(tupled[-1])
